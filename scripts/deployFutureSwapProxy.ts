22bd7185048c1ea20b988dc59207ef758098afc0
Gerben <stavenga@gmail.com>
2022-12-09 14:38:18 -0800

    start deploy

--- scripts/deployFutureSwapProxy.ts
+++ scripts/deployFutureSwapProxy.ts
@@ -2,31 +2,21 @@ import type {IAnyswapCreate2Deployer} from "../typechain-types";
 
 import {ethers} from "hardhat";
 
-import {FUTURESWAP_DEPLOYER_MNEMONIC} from "../hardhat.config";
 import {deployAtFixedAddress, deployGovernanceProxy, fsSalt} from "../lib/deploy";
 import {getAddressesForNetwork, getContracts, saveAddressesForNetwork} from "../lib/deployment";
 import {FutureSwapProxy__factory} from "../typechain-types";
 
 async function main() {
   const [workDeployer] = await ethers.getSigners();
-  const fsDeployer = ethers.Wallet.fromMnemonic(FUTURESWAP_DEPLOYER_MNEMONIC).connect(
-    ethers.provider,
-  );
   const networkAddresses = await getAddressesForNetwork();
   const networkContracts = getContracts(networkAddresses, workDeployer);
   const anyswapCreate2Deployer = networkContracts.anyswapCreate2Deployer as IAnyswapCreate2Deployer;
   const futureSwapProxy = await deployAtFixedAddress(
-    new FutureSwapProxy__factory(fsDeployer),
+    new FutureSwapProxy__factory(workDeployer),
     anyswapCreate2Deployer,
     fsSalt,
-    fsDeployer.address,
+    workDeployer.address,  // This should become FutureSwap team address.
   );
-  await (
-    await workDeployer.sendTransaction({
-      to: fsDeployer.address,
-      value: ethers.utils.parseEther("0.01"),
-    })
-  ).wait();
   const governanceProxy = await deployGovernanceProxy(
     futureSwapProxy.address,
     anyswapCreate2Deployer,
