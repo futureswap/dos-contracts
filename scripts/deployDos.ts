66125d2f15609a7ad88c51a721cbcdea982b19d3
Gerben <stavenga@gmail.com>
2022-12-08 19:11:44 -0800

    format

--- scripts/deployDos.ts
+++ scripts/deployDos.ts
@@ -1,9 +1,11 @@
-import {ethers} from "hardhat";
-import { makeCall } from "../lib/calls";
+import type {GovernanceProxy, IAnyswapCreate2Deployer} from "../typechain-types";
 
+import {ethers} from "hardhat";
+
+import {makeCall} from "../lib/calls";
 import {deployAtFixedAddress, fsSalt} from "../lib/deploy";
 import {getAddressesForNetwork, getContracts, saveAddressesForNetwork} from "../lib/deployment";
-import { DOS__factory, VersionManager__factory, GovernanceProxy, IAnyswapCreate2Deployer, PortfolioLogic__factory } from "../typechain-types";
+import {DOS__factory, VersionManager__factory, PortfolioLogic__factory} from "../typechain-types";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -11,9 +13,25 @@ async function main() {
   const networkContracts = getContracts(networkAddresses, deployer);
   const anyswapCreate2Deployer = networkContracts.anyswapCreate2Deployer as IAnyswapCreate2Deployer;
   const governanceProxy = networkContracts.governanceProxy as GovernanceProxy;
-  const versionManager = await deployAtFixedAddress(new VersionManager__factory(deployer), anyswapCreate2Deployer, fsSalt, governanceProxy.address);
-  const dos = await deployAtFixedAddress(new DOS__factory(deployer), anyswapCreate2Deployer, fsSalt, governanceProxy.address, versionManager.address);
-  const portfolioLogic = await deployAtFixedAddress(new PortfolioLogic__factory(deployer), anyswapCreate2Deployer, fsSalt, dos.address);
+  const versionManager = await deployAtFixedAddress(
+    new VersionManager__factory(deployer),
+    anyswapCreate2Deployer,
+    fsSalt,
+    governanceProxy.address,
+  );
+  const dos = await deployAtFixedAddress(
+    new DOS__factory(deployer),
+    anyswapCreate2Deployer,
+    fsSalt,
+    governanceProxy.address,
+    versionManager.address,
+  );
+  const portfolioLogic = await deployAtFixedAddress(
+    new PortfolioLogic__factory(deployer),
+    anyswapCreate2Deployer,
+    fsSalt,
+    dos.address,
+  );
   await governanceProxy.execute([
     makeCall(versionManager, "addVersion", ["1.0.0", 2, portfolioLogic.address]),
     makeCall(versionManager, "markRecommendedVersion", ["1.0.0"]),
