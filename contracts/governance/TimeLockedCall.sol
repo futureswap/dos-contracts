3ed2c9443dd4eec58e35fd0129dae64f44289ad5
Gerben <stavenga@gmail.com>
2022-12-25 13:30:09 -0800

    improvements

--- contracts/governance/TimeLockedCall.sol
+++ contracts/governance/TimeLockedCall.sol
@@ -4,47 +4,51 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "./GovernanceProxy.sol";
 import "../lib/ImmutableGovernance.sol";
+import "../tokens/HashNFT.sol";
 
 contract TimeLockedCall is ImmutableGovernance, Ownable2Step {
+    HashNFT public immutable hashNFT;
     uint8 public immutable accessLevel;
 
     uint256 public lockTime;
-    // Bit complemented execution time. This makes the default value 0
-    // mean uint256.max, which is the larger then any possible timestamp.
-    mapping(bytes32 => uint256) public bitComplementedExecutionTime;
 
-    event BatchProposed(CallWithoutValue[] calls, bytes32 salt, uint256 executionTime);
+    event BatchProposed(CallWithoutValue[] calls, uint256 executionTime);
 
     constructor(
         address governance,
+        address hashNFT_,
         uint8 _accessLevel,
         uint256 _lockTime
     ) ImmutableGovernance(governance) {
         accessLevel = _accessLevel;
+        hashNFT = HashNFT(FsUtils.nonNull(hashNFT_));
         lockTime = _lockTime;
     }
 
-    function proposeBatch(CallWithoutValue[] calldata calls, bytes32 salt) external onlyOwner {
-        bytes32 hash = keccak256(abi.encode(salt, calls));
-        require(bitComplementedExecutionTime[hash] == 0, "TimeLockedCall: already proposed");
-        bitComplementedExecutionTime[hash] = ~(block.timestamp + lockTime);
-        emit BatchProposed(calls, salt, block.timestamp + lockTime);
+    function proposeBatch(CallWithoutValue[] calldata calls) external onlyOwner {
+        uint256 executionTime = block.timestamp + lockTime;
+        hashNFT.mint(address(this), calcDigest(calls, executionTime), "");
+        emit BatchProposed(calls, block.timestamp + lockTime);
     }
 
-    function executeBatch(CallWithoutValue[] calldata calls, bytes32 salt) external {
-        bytes32 hash = keccak256(abi.encode(salt, calls));
-        require(
-            ~bitComplementedExecutionTime[hash] <= block.timestamp,
-            "TimeLockedCall: not ready"
-        );
+    function executeBatch(CallWithoutValue[] calldata calls, uint256 executionTime) external {
+        require(executionTime <= block.timestamp, "TimeLockedCall: not ready");
+        uint256 tokenId = hashNFT.toTokenId(address(this), calcDigest(calls, executionTime));
+        hashNFT.burn(address(this), tokenId, 1);
         Governance(GovernanceProxy(immutableGovernance).governance()).executeBatchWithClearance(
             calls,
             accessLevel
         );
-        bitComplementedExecutionTime[hash] = 0;
     }
 
     function setLockTime(uint256 _lockTime) external onlyGovernance {
         lockTime = _lockTime;
+    }
+
+    function calcDigest(
+        CallWithoutValue[] calldata calls,
+        uint256 executionTime
+    ) internal pure returns (bytes32) {
+        return keccak256(abi.encode(executionTime, calls));
     }
 }
