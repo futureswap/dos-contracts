0a8deee0c5671acb2b48eb017a92fd0f70903884
Gerben <stavenga@gmail.com>
2022-12-29 18:47:03 -0800

    Fix type error + enable type check on CI

--- scripts/resetEtheralWorkspace.ts
+++ scripts/resetEtheralWorkspace.ts
@@ -1,8 +1,8 @@
 import hre from "hardhat";
 
 async function main() {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  await hre.ethernal.resetWorkspace("test");
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
+  await (hre as any).ethernal.resetWorkspace("test");
 }
 
 main().catch(error => {
