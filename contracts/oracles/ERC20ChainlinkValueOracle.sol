60769c307e0a705389f6761fdffb1e483dcf3a38
SonOfMosiah <dev@sonofmosiah.ai>
2023-01-12 19:36:19 -0700

    Add immutableGovernance + collateralFactor

--- contracts/oracles/ERC20ChainlinkValueOracle.sol
+++ contracts/oracles/ERC20ChainlinkValueOracle.sol
@@ -5,19 +5,33 @@ import "../lib/FsUtils.sol";
 import "../interfaces/IERC20ValueOracle.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract ERC20ChainlinkValueOracle is IERC20ValueOracle {
+import {ImmutableGovernance} from "../lib/ImmutableGovernance.sol";
+
+contract ERC20ChainlinkValueOracle is ImmutableGovernance, IERC20ValueOracle {
     AggregatorV3Interface priceOracle;
     int256 immutable base;
+    int256 collateralFactor;
 
-    constructor(address chainlink, uint8 baseDecimals, uint8 tokenDecimals) {
+    constructor(
+        address chainlink,
+        uint8 baseDecimals,
+        uint8 tokenDecimals,
+        int256 _collateralFactor,
+        address _owner
+    ) ImmutableGovernance(_owner) {
         priceOracle = AggregatorV3Interface(FsUtils.nonNull(chainlink));
         base = int256(10) ** (tokenDecimals + priceOracle.decimals() - baseDecimals);
+        collateralFactor = _collateralFactor;
+    }
+
+    function setCollateralFactor(int256 _collateralFactor) external onlyGovernance {
+        collateralFactor = _collateralFactor;
     }
 
     function calcValue(int256 balance) external view override returns (int256, int256) {
         (, int256 price, , , ) = priceOracle.latestRoundData();
         int256 value = (balance * price) / base;
-        int256 riskAdjustedValue;
+        int256 riskAdjustedValue = (value * collateralFactor) / 1 ether;
         return (value, riskAdjustedValue);
     }
 }
