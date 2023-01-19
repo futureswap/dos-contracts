5c861e2a850dcf1a9122a3f669c9de54a687613e
SonOfMosiah <dev@sonofmosiah.ai>
2023-01-18 17:46:44 -0700

    FIX: forge test

--- contracts/duoswapV2/libraries/DuoswapV2Library.sol
+++ contracts/duoswapV2/libraries/DuoswapV2Library.sol
@@ -31,7 +31,7 @@ library DuoswapV2Library {
                             bytes1(0xff),
                             factory,
                             keccak256(abi.encodePacked(token0, token1)),
-                            hex"6606d7ddc46633fe75181c4673be2eb17f1a8360674e23ec54231cec1e5077c6" // NOTE: need to replace with actual init code hash every time the pair code is updated
+                            hex"15e3b2483cec6ad9dfd8072681caa064afa091676218c2f7abe86889419d3e74" // NOTE: need to replace with actual init code hash every time the pair code is updated
                         )
                     )
                 )
