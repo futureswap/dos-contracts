99724c1391c46fd9c52e86b6f4ba3bf82fb3a0bf
SonOfMosiah <dev@sonofmosiah.ai>
2023-01-24 18:32:24 -0700

    Add collateralFactor event on NFT oracle

--- contracts/interfaces/INFTValueOracle.sol
+++ contracts/interfaces/INFTValueOracle.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.17;
 
 /// @title NFT Value Oracle Interface
 interface INFTValueOracle {
+    /// @notice Emitted when collateral factor is set
+    /// @param collateralFactor Collateral factor
+    event CollateralFactorSet(int256 indexed collateralFactor);
+
     function calcValue(
         uint256 tokenId
     ) external view returns (int256 value, int256 riskAdjustedValue);
