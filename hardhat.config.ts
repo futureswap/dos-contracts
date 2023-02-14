ee16c39eaad1f396f0600c784ab9849caed7eec7
SonOfMosiah <dev@sonofmosiah.ai>
2023-02-14 09:50:19 -0700

    Use solc 0.8.18

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -47,7 +47,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
+        version: "0.8.18",
         settings: {
           metadata: {
             // not including the metadata hash
